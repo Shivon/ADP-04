ccfff197506479ead0d74d029ed89e645ba388e3
Marjan Bachtiari <marjan.bachtiari@haw-hamburg.de>
2015-01-16 17:12:20 +0100

    Add predicate isBalanced and function balance - TODO: implement the rotations

--- src/avlTree.erl
+++ src/avlTree.erl
@@ -10,8 +10,7 @@
 -author("KamikazeOnRoad").
 
 %% API
--export([intitTree/0, addToTree/2]).
-
+-export([intitTree/0, addToTree/2, isBalanced/1]).
 
 %% leftChild(P) rightChild(P)
 
@@ -48,16 +47,72 @@ initNode(Number) -> [1, 1, Number, nil, nil].
 %% Empty tree => HeightLeft and HeightRight = 0,
 %% Tree with only root => HeightLeft and HeightRight = 1
 addToTree(Number, [0, 0, nil, nil, nil]) -> [1, 1, Number, nil, nil];
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, nil, nil]) when Number < Parent ->
-  [HeightLeft + 1, HeightRight, Parent, initNode(Number), nil];
+  balance([HeightLeft + 1, HeightRight, Parent, initNode(Number), nil]);
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, nil, nil]) when Number >= Parent ->
-  [HeightLeft, HeightRight + 1, Parent, nil, initNode(Number)];
+  balance([HeightLeft, HeightRight + 1, Parent, nil, initNode(Number)]);
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, nil, ChildRight]) when Number < Parent ->
-  [HeightLeft, HeightRight, Parent, initNode(Number), ChildRight];
+  balance([HeightLeft + 1, HeightRight, Parent, initNode(Number), ChildRight]);
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, ChildLeft, nil]) when Number >= Parent ->
-  [HeightLeft, HeightRight, Parent, ChildLeft, initNode(Number)];
-%% TODO balance!!
+  balance([HeightLeft, HeightRight + 1, Parent, ChildLeft, initNode(Number)]);
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, ChildLeft, ChildRight]) when Number < Parent ->
   [HeightLeft + 1, HeightRight, Parent, addToTree(Number, ChildLeft), ChildRight];
+
 addToTree(Number, [HeightLeft, HeightRight, Parent, ChildLeft, ChildRight]) when Number >= Parent ->
   [HeightLeft, HeightRight + 1, Parent, ChildLeft, addToTree(Number, ChildRight)].
+
+
+
+%% Checks if tree is balanced and rotates if it isnt
+balance([Height, Height, Parent, nil, nil]) -> [Height, Height, Parent, nil, nil];
+balance([HeightLeft, HeightRight, Parent, nil, ChildRight]) ->
+  Tree = [HeightLeft, HeightRight, Parent, nil, ChildRight],
+  Difference = HeightRight - HeightLeft,
+  if
+    Difference < -1 -> rotateRight(Tree);
+    Difference > 1 -> rotateLeft(Tree);
+    true -> balance(ChildRight)
+  end;
+balance([HeightLeft, HeightRight, Parent, ChildLeft, nil]) ->
+  Tree = [HeightLeft, HeightRight, Parent, ChildLeft, nil],
+  Difference = HeightRight - HeightLeft,
+  if
+    Difference < -1 -> rotateRight(Tree);
+    Difference > 1 -> rotateLeft(Tree);
+    true -> balance(ChildLeft)
+  end;
+balance([HeightLeft, HeightRight, Parent, ChildLeft, ChildRight]) ->
+  [HeightLeft, HeightRight, Parent, balance(ChildLeft), balance(ChildRight)].
+
+
+
+%% Prädikat
+isBalanced([Height, Height, _, nil, nil]) -> true;
+isBalanced([HeightLeft, HeightRight, _, nil, ChildRight]) ->
+    Difference = HeightRight - HeightLeft,
+    if
+      Difference < -1 -> false;
+      Difference > 1 -> false;
+      true -> isBalanced(ChildRight)
+    end;
+isBalanced([HeightLeft, HeightRight, _, ChildLeft, nil]) ->
+  Difference = HeightRight - HeightLeft,
+  if
+    Difference < -1 -> false;
+    Difference > 1 -> false;
+    true -> isBalanced(ChildLeft)
+  end;
+isBalanced([HeightLeft, HeightRight, _, ChildLeft, ChildRight]) ->
+  Difference = HeightRight - HeightLeft,
+  if
+    Difference < -1 -> false;
+    Difference > 1 -> false;
+    true -> isBalanced(ChildLeft) and isBalanced(ChildRight)
+  end.
+
+%%isUnbalanced(Tree) -> not(isBalanced(Tree)).
